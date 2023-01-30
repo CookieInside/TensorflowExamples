75ca1dbf4472759e137b31259b22a500afdd9ea3
CookieInside <tim.olges@gmx.de>
2023-01-30 19:10:50 +0100

    image transformation

--- ComputerVision.py
+++ ComputerVision.py
@@ -1,13 +1,15 @@
 import tensorflow as tf
+import scipy
 from tensorflow.keras import datasets, layers, models
+from tensorflow.keras.utils import img_to_array
 import matplotlib.pyplot as plt
 from keras.preprocessing import image
 from keras.preprocessing.image import ImageDataGenerator
 
-# Laden und Aufteilen der Datensätze
+# Laden und Aufteilen der Datensaetze
 (trainImages, trainLabels),(testImages, testLabels) = datasets.cifar10.load_data()
 
-# Pixelwerte zwischen 1 und 0 einschränken
+# Pixelwerte zwischen 1 und 0 einschraenken
 trainImages, testImages = trainImages / 255.0, testImages / 255.0
 
 # Klassennamen
@@ -19,7 +21,7 @@ classNames = ["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "h
 #plt.xlabel(classNames[trainLabels[ImgIndex][0]])
 #plt.show()
 
-# Transformiert das Bild um es mehrfach nutzen zu können ohne Dopplungen in kauf nehmen zu müssen
+# Transformiert das Bild um es mehrfach nutzen zu koennen ohne Dopplungen in kauf nehmen zu muessen
 
 datagen = ImageDataGenerator(
     rotation_range=40,
@@ -31,6 +33,21 @@ datagen = ImageDataGenerator(
     fill_mode="nearest"
 )
 
+testImg = trainImages[14]
+img = img_to_array(testImg)
+img = img.reshape((1,) + img.shape)
+
+i = 0
+
+for batch in datagen.flow(img, save_prefix="test", save_format="jpeg"):
+    plt.figure(i)
+    plot = plt.imshow(img_to_array(batch[0]))
+    i += 1
+    if i > 4:
+        break
+
+plt.show()
+
 # Aufbau der konvolutionellen Basis des Models
 model = models.Sequential()
 model.add(layers.Conv2D(32, (3, 3), activation="relu", input_shape=(32, 32, 3)))
@@ -39,8 +56,20 @@ model.add(layers.Conv2D(64, (3, 3), activation="relu"))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation="relu"))
 
-# Hinzufügen der dichten Schichten
+# Hinzufuegen der dichten Schichten
 
 model.add(layers.Flatten())                     # Konvertiert die Pixelwertmatix in ein eindimensonales numpy Array
 model.add(layers.Dense(64, activation="relu"))  # Siehe neuralNetwork l.19
 model.add(layers.Dense(10))                     # Siehe neuralNetwork l.20
+
+model.compile(
+    optimizer="adam",
+    loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=["accuracy"]
+)
+history = model.fit(
+    trainImages,
+    trainLabels,
+    epochs=10,
+    validation_data=(testImages, testLabels)
+)
