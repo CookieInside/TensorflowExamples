ace93167c16704301170066c1da6796d870783d6
CookieInside <tim.olges@gmx.de>
2023-03-15 13:06:58 +0100

    textClassifing fertig, nicht 100% funktionsfähig

--- TextClassification.py
+++ TextClassification.py
@@ -10,28 +10,6 @@ VOCAB_SIZE = 88584
 MAX_LENGTH = 250 # Anzahl der Wörter pro Text, weitere Wörter werden entfernt und bei einem zu korzen Text werden Wörter mit dem Wert 0 eingefügt
 BATCH_SIZE = 64
 
-def trainModel():
-    (trainData, trainLabels), (testData, testLabels) = imdb.load_data(num_words= VOCAB_SIZE) # Laden des Datensatzes
-    trainData = pad_sequences(trainData, MAX_LENGTH)
-    testData = pad_sequences(testData, MAX_LENGTH)
-
-    # Erstellen des Models
-    model = tf.keras.Sequential([
-    tf.keras.layers.Embedding(VOCAB_SIZE, 32),
-    tf.keras.layers.LSTM(32),
-    tf.keras.layers.Dense(1, activation="sigmoid")
-    ])
-    model.save("./models/textClassification.h5")
-
-    model.compile(loss="binary_crossentropy", optimizer="rmsprop", metrics=["acc"]) # Trainigsfunktionen auswählen
-    history = model.fit(trainData, trainLabels, epochs=10, validation_split=0.2) # Trainiren des Models
-
-    results = model.evaluate(testData, testLabels) # Model anhand des Test-Datensatzen testen
-    print(results) # Test-Ergebnisse ausgeben
-
-# trainModel()
-
-
 # Text kodierer
 wordIndex = imdb.get_word_index() # Liste aller Wörter die im Training genutzt worden
 
@@ -51,12 +29,34 @@ def decodeInt(integers):
             text += reverseWordIndex[num] + " " # Suche das Wort für den Wert in der Map
     return text[:-1] # Gib alles zurück bis auf das letzte Zeichen (hier ein unnötiges Leerzeichen)
 
+
+def trainModel():
+    (trainData, trainLabels), (testData, testLabels) = imdb.load_data(num_words= VOCAB_SIZE) # Laden des Datensatzes
+    trainData = pad_sequences(trainData, MAX_LENGTH)
+    testData = pad_sequences(testData, MAX_LENGTH)
+    print(decodeInt(testData[0]))
+    # Erstellen des Models
+    model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(VOCAB_SIZE, 32),
+    tf.keras.layers.LSTM(32),
+    tf.keras.layers.Dense(1, activation="sigmoid")
+    ])
+    model.save("./models/textClassification.h5")
+
+    model.compile(loss="binary_crossentropy", optimizer="rmsprop", metrics=["acc"]) # Trainigsfunktionen auswählen
+    history = model.fit(trainData, trainLabels, epochs=10, validation_split=0.2, batch_size=BATCH_SIZE) # Trainiren des Models
+
+    results = model.evaluate(testData, testLabels) # Model anhand des Test-Datensatzen testen
+    print(results) # Test-Ergebnisse ausgeben
+
+#trainModel()
+
 def predict(text):
     encoded = encodeText(text) # text kodieren
     pred = np.zeros((1,250)) # Basis Array erstellen
     pred[0] = encoded # Text in Array einfügen
     result = model.predict(pred) # Probe mit Model vorhersagen lassen
-    print(result[0])
+    print(result[0][0])
 
 model = tf.keras.models.load_model("./models/textClassification.h5")
 
