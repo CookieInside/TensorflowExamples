224c8f28f57d206c32c1b3ac79b63004e0099d5c
CookieInside <tim.olges@gmx.de>
2023-05-28 19:54:35 +0200

    gpu support

--- TextClassification.py
+++ TextClassification.py
@@ -6,67 +6,68 @@ import tensorflow as tf
 import os
 import numpy as np
 
-VOCAB_SIZE = 88584
-MAX_LENGTH = 250 # Anzahl der Wörter pro Text, weitere Wörter werden entfernt und bei einem zu korzen Text werden Wörter mit dem Wert 0 eingefügt
-BATCH_SIZE = 64
+with tf.device("/GPU:0"):
+    VOCAB_SIZE = 88584
+    MAX_LENGTH = 250 # Anzahl der Wörter pro Text, weitere Wörter werden entfernt und bei einem zu korzen Text werden Wörter mit dem Wert 0 eingefügt
+    BATCH_SIZE = 64
 
-# Text Kodierer
-wordIndex = imdb.get_word_index() # Liste aller Wörter die im Training genutzt worden
+    # Text Kodierer
+    wordIndex = imdb.get_word_index() # Liste aller Wörter die im Training genutzt worden
 
-def encodeText(text):
-    tokens = keras.preprocessing.text.text_to_word_sequence(text) # Umwandeln des Strings in eine Liste aus Worten
-    tokens = [wordIndex[word] if word in wordIndex else 0 for word in tokens] # Wörter durch Nummern ersetzten
-    return pad_sequences([tokens], MAX_LENGTH)[0]
+    def encodeText(text):
+        tokens = keras.preprocessing.text.text_to_word_sequence(text) # Umwandeln des Strings in eine Liste aus Worten
+        tokens = [wordIndex[word] if word in wordIndex else 0 for word in tokens] # Wörter durch Nummern ersetzten
+        return pad_sequences([tokens], MAX_LENGTH)[0]
 
-# Text Dekodierer
-reverseWordIndex = {value: key for (key, value) in wordIndex.items()} # eine Map mit Nummer und entsprechenden Wort
+    # Text Dekodierer
+    reverseWordIndex = {value: key for (key, value) in wordIndex.items()} # eine Map mit Nummer und entsprechenden Wort
 
-def decodeInt(integers):
-    PAD = 0 # Null ist der Füller-Wert
-    text = "" # Basis des Strings
-    for num in integers: # Durch die Liste an Nummern loopen
-        if num != PAD: # wenn es kein Füller-Wert ist
-            text += reverseWordIndex[num] + " " # Suche das Wort für den Wert in der Map
-    return text[:-1] # Gib alles zurück bis auf das letzte Zeichen (hier ein unnötiges Leerzeichen)
+    def decodeInt(integers):
+        PAD = 0 # Null ist der Füller-Wert
+        text = "" # Basis des Strings
+        for num in integers: # Durch die Liste an Nummern loopen
+            if num != PAD: # wenn es kein Füller-Wert ist
+                text += reverseWordIndex[num] + " " # Suche das Wort für den Wert in der Map
+        return text[:-1] # Gib alles zurück bis auf das letzte Zeichen (hier ein unnötiges Leerzeichen)
 
 
-def trainModel():
-    (trainData, trainLabels), (testData, testLabels) = imdb.load_data(num_words= VOCAB_SIZE) # Laden des Datensatzes
-    trainData = pad_sequences(trainData, MAX_LENGTH)
-    testData = pad_sequences(testData, MAX_LENGTH)
-    print(decodeInt(testData[0]))
-    # Erstellen des Models
-    model = tf.keras.Sequential([
-    tf.keras.layers.Embedding(VOCAB_SIZE, 32),
-    tf.keras.layers.LSTM(32),
-    tf.keras.layers.Dense(1, activation="sigmoid")
-    ])
+    def trainModel():
+        (trainData, trainLabels), (testData, testLabels) = imdb.load_data(num_words= VOCAB_SIZE) # Laden des Datensatzes
+        trainData = pad_sequences(trainData, MAX_LENGTH)
+        testData = pad_sequences(testData, MAX_LENGTH)
+        print(decodeInt(testData[0]))
+        # Erstellen des Models
+        model = tf.keras.Sequential([
+        tf.keras.layers.Embedding(VOCAB_SIZE, 32),
+        tf.keras.layers.LSTM(32),
+        tf.keras.layers.Dense(1, activation="sigmoid")
+        ])
 
-    model.compile(loss="binary_crossentropy", optimizer="rmsprop", metrics=["acc"]) # Trainigsfunktionen auswählen
-    history = model.fit(trainData, trainLabels, epochs=10, validation_split=0.2, batch_size=BATCH_SIZE) # Trainiren des Models
+        model.compile(loss="binary_crossentropy", optimizer="rmsprop", metrics=["acc"]) # Trainigsfunktionen auswählen
+        history = model.fit(trainData, trainLabels, epochs=10, validation_split=0.2, batch_size=BATCH_SIZE) # Trainiren des Models
 
-    results = model.evaluate(testData, testLabels) # Model anhand des Test-Datensatzen testen
-    print(results) # Test-Ergebnisse ausgeben
-    model.save("./models/textClassification.h5")
-#trainModel()
+        results = model.evaluate(testData, testLabels) # Model anhand des Test-Datensatzen testen
+        print(results) # Test-Ergebnisse ausgeben
+        model.save("./models/textClassification.h5")
+    trainModel()
 
-def predict(text):
-    encoded = encodeText(text) # text kodieren
-    pred = np.zeros((1,250)) # Basis Array erstellen
-    pred[0] = encoded # Text in Array einfügen
-    result = model.predict(pred) # Probe mit Model vorhersagen lassen
-    if result[0][0] > 0.5:
-        print(f"positive ({result[0][0]})")
-    else:
-        print(f"negative ({result[0][0]})")
+    def predict(text):
+        encoded = encodeText(text) # text kodieren
+        pred = np.zeros((1,250)) # Basis Array erstellen
+        pred[0] = encoded # Text in Array einfügen
+        result = model.predict(pred) # Probe mit Model vorhersagen lassen
+        if result[0][0] > 0.5:
+            print(f"positive ({result[0][0]})")
+        else:
+            print(f"negative ({result[0][0]})")
 
-model = tf.keras.models.load_model("./models/textClassification.h5")
+    model = tf.keras.models.load_model("./models/textClassification.h5")
 
-using = True
-print("input 'stop' to stop testing, the output number means positive if it is greater than .5 and negative if it is smaller")
-while using:
-    text = input("Input your movie review: ")
-    if(text == "stop"):
-        using = False
-    else:
-        predict(text)
+    using = True
+    print("input 'stop' to stop testing, the output number means positive if it is greater than .5 and negative if it is smaller")
+    while using:
+        text = input("Input your movie review: ")
+        if(text == "stop"):
+            using = False
+        else:
+            predict(text)
--- TextGeneration.py
+++ TextGeneration.py
@@ -4,100 +4,101 @@ import tensorflow as tf
 import os
 import numpy as np
 
-pathToFile = keras.utils.get_file("shakespeare.txt", "https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt") # Eine Textdatei laden, in diesem Fall Shakespeares Coriolanus
+with tf.device("/GPU:0"):
+    pathToFile = keras.utils.get_file("shakespeare.txt", "https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt") # Eine Textdatei laden, in diesem Fall Shakespeares Coriolanus
 
-text = open(pathToFile, "rb").read().decode(encoding="utf-8") # Text als String im read bytes (rb) Modus auslesen und als UTF-8 decodieren
-print(f"Length of text: {len(text)} characters") # Länge des Textes ausgeben
+    text = open(pathToFile, "rb").read().decode(encoding="utf-8") # Text als String im read bytes (rb) Modus auslesen und als UTF-8 decodieren
+    print(f"Length of text: {len(text)} characters") # Länge des Textes ausgeben
 
-print(text[:2500])
+    print(text[:2500])
 
-vocab = sorted(set(text)) # Erstellt eine Liste aus allem Buchstaben des Textes
-char2idx = {u:i for i, u in enumerate(vocab)} # Erstellt ein Map mit Buchstaben und Index in der Liste (benötigt um vom Buchstaben zum Index zu kommen)
-idx2char = np.array(vocab) # Erstellt ein Array mit allem Buchstaben des Textes (benötigt um von Index zum Buchstaben zu kommen)
+    vocab = sorted(set(text)) # Erstellt eine Liste aus allem Buchstaben des Textes
+    char2idx = {u:i for i, u in enumerate(vocab)} # Erstellt ein Map mit Buchstaben und Index in der Liste (benötigt um vom Buchstaben zum Index zu kommen)
+    idx2char = np.array(vocab) # Erstellt ein Array mit allem Buchstaben des Textes (benötigt um von Index zum Buchstaben zu kommen)
 
-def textToInt(text):
-    return np.array([char2idx[c] for c in text]) # Gibt ein Array mit den Indexen der Buchstaben eines Textes zurück
+    def textToInt(text):
+        return np.array([char2idx[c] for c in text]) # Gibt ein Array mit den Indexen der Buchstaben eines Textes zurück
 
-def intToText(ints):
-    try:
-        ints = ints.numpy() # Wandle in eine numpy-Array um wenn es nicht schon eins ist
-    except:
-        pass
-    return "".join(idx2char[ints]) # Buchstaben einzeln anhägen und dann als String zurückgeben
+    def intToText(ints):
+        try:
+            ints = ints.numpy() # Wandle in eine numpy-Array um wenn es nicht schon eins ist
+        except:
+            pass
+        return "".join(idx2char[ints]) # Buchstaben einzeln anhägen und dann als String zurückgeben
 
-textAsInt = textToInt(text)
+    textAsInt = textToInt(text)
 
-seqLength = 100 # Länge einer Trainings-Sequenz
-examplesPerEpoch = len(text)//(seqLength+1) # Beispiele pro Epoche: die Länge des Textes geteilt durch die Länge einer Trainings-Sequenz plus eins
+    seqLength = 100 # Länge einer Trainings-Sequenz
+    examplesPerEpoch = len(text)//(seqLength+1) # Beispiele pro Epoche: die Länge des Textes geteilt durch die Länge einer Trainings-Sequenz plus eins
 
-charDataset = tf.data.Dataset.from_tensor_slices(textAsInt) # Wandelt String Dataset zu Tensorflow Dataset um
+    charDataset = tf.data.Dataset.from_tensor_slices(textAsInt) # Wandelt String Dataset zu Tensorflow Dataset um
 
-sequences = charDataset.batch(seqLength+1, drop_remainder=True) # Die letzten Zeichen des Datensatzes, die keine ganze Sequenz auffüllen können, werden nicht genutzt
+    sequences = charDataset.batch(seqLength+1, drop_remainder=True) # Die letzten Zeichen des Datensatzes, die keine ganze Sequenz auffüllen können, werden nicht genutzt
 
-def splitToInputAndTarget(chunk): # Beispiel: chunk = "hello"
-    input = chunk[:-1]            #           input = "hell"
-    target = chunk[1:]            #          target = "ello"
-    return input, target
+    def splitToInputAndTarget(chunk): # Beispiel: chunk = "hello"
+        input = chunk[:-1]            #           input = "hell"
+        target = chunk[1:]            #          target = "ello"
+        return input, target
 
-dataset = sequences.map(splitToInputAndTarget) # Split-Funktion auf den Datensatz anwenden
-# Der Datensatz hat nun sowohl einen input als auch einen Target Anteil: der input Anteil ist der gegebene Wert und Target der Zielwert
+    dataset = sequences.map(splitToInputAndTarget) # Split-Funktion auf den Datensatz anwenden
+    # Der Datensatz hat nun sowohl einen input als auch einen Target Anteil: der input Anteil ist der gegebene Wert und Target der Zielwert
 
-# Erstellen von Training-Batches
-BATCH_SIZE = 64 # Größe eines Batches
-VOCAB_SIZE = len(vocab) # anzahl aller Charakter
-EMBEDDING_DIM = 256 # Anzahl der Dimensionen des Vektors der die Worte repräsentiert
-RNN_UNITS = 1024 # 
-BUFFER_SIZE = 10000 # Größe des Buffers: Tensorflow speichert das Dataset in einem Buffer, da es von einem Theoretisch unendlich großem Dataset ausgeht und somit die Sequenz nicht im Ram shuffelt
+    # Erstellen von Training-Batches
+    BATCH_SIZE = 64 # Größe eines Batches
+    VOCAB_SIZE = len(vocab) # anzahl aller Charakter
+    EMBEDDING_DIM = 256 # Anzahl der Dimensionen des Vektors der die Worte repräsentiert
+    RNN_UNITS = 1024 # 
+    BUFFER_SIZE = 10000 # Größe des Buffers: Tensorflow speichert das Dataset in einem Buffer, da es von einem Theoretisch unendlich großem Dataset ausgeht und somit die Sequenz nicht im Ram shuffelt
 
-data = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True) # Datensatz shuffeln (in zufällige Reihenfolge bringen)
-# Nach dieser Interaktion mit dem Datensatz gibt es Batches, die aus 64 Sequenzen bestehen
+    data = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True) # Datensatz shuffeln (in zufällige Reihenfolge bringen)
+    # Nach dieser Interaktion mit dem Datensatz gibt es Batches, die aus 64 Sequenzen bestehen
 
-# Modell bauen:
-# Das Modell-Bauen wird mit einer Methode ungesetzt, damit zum Trainieren ein Modell erstellt werden kann, dass direkt 64 Sequenzen auf einmal verarbeitet, später, bei der Nutzung des Modells wird auf das Trainierte Modell zurückgegriffen, dieses jedoch neu konstruiert um immer nur eine Sequenz auf einmal zu verarbeiten
-def buildModel(vocabSize, embeddingDim, rnnUnits, batchSize):
-    model = tf.keras.Sequential([
-        tf.keras.layers.Embedding(
-            vocabSize,
-            embeddingDim,
-            batch_input_shape=[batchSize, None] # Die Batch-Größe ist bekannt, die Sequenz-Größe jedoch nicht
-        ),
-        tf.keras.layers.LSTM(
-            rnnUnits,
-            return_sequences=True, # Alle vorherrgesagten Sequenzen werden zurückgegeben
-            stateful=True,
-            recurrent_initializer="glorot_uniform"
-        ),
-        tf.keras.layers.Dense(vocabSize)    # Output-Layer: Wahrscheinlichkeit für jeden Charakter
-    ])
-    return model
+    # Modell bauen:
+    # Das Modell-Bauen wird mit einer Methode ungesetzt, damit zum Trainieren ein Modell erstellt werden kann, dass direkt 64 Sequenzen auf einmal verarbeitet, später, bei der Nutzung des Modells wird auf das Trainierte Modell zurückgegriffen, dieses jedoch neu konstruiert um immer nur eine Sequenz auf einmal zu verarbeiten
+    def buildModel(vocabSize, embeddingDim, rnnUnits, batchSize):
+        model = tf.keras.Sequential([
+            tf.keras.layers.Embedding(
+                vocabSize,
+                embeddingDim,
+                batch_input_shape=[batchSize, None] # Die Batch-Größe ist bekannt, die Sequenz-Größe jedoch nicht
+            ),
+            tf.keras.layers.LSTM(
+                rnnUnits,
+                return_sequences=True, # Alle vorherrgesagten Sequenzen werden zurückgegeben
+                stateful=True,
+                recurrent_initializer="glorot_uniform"
+            ),
+            tf.keras.layers.Dense(vocabSize)    # Output-Layer: Wahrscheinlichkeit für jeden Charakter
+        ])
+        return model
 
-model = buildModel(VOCAB_SIZE, EMBEDDING_DIM, RNN_UNITS, BATCH_SIZE)
+    model = buildModel(VOCAB_SIZE, EMBEDDING_DIM, RNN_UNITS, BATCH_SIZE)
 
-for inputExampleBatch, targetExampleBatch in data.take(1):
-    exampleBatchPredictions = model(inputExampleBatch) # Das Modell einen Wert vorherrsagen lassen
-    #print(exampleBatchPredictions.shape, "# (batch_size, sequenze_length, vocab_size)")
+    for inputExampleBatch, targetExampleBatch in data.take(1):
+        exampleBatchPredictions = model(inputExampleBatch) # Das Modell einen Wert vorherrsagen lassen
+        #print(exampleBatchPredictions.shape, "# (batch_size, sequenze_length, vocab_size)")
 
-#print(len(exampleBatchPredictions))
-#print(exampleBatchPredictions)
+    #print(len(exampleBatchPredictions))
+    #print(exampleBatchPredictions)
 
-#pred = exampleBatchPredictions[0]
-#print(len(pred))
-#print(pred)
+    #pred = exampleBatchPredictions[0]
+    #print(len(pred))
+    #print(pred)
 
-def loss(labels, logits):
-    return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)    # Keras loss-Funktion nutzen um eigene loss Funktion zu erstellen
+    def loss(labels, logits):
+        return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)    # Keras loss-Funktion nutzen um eigene loss Funktion zu erstellen
 
-model.compile(optimizer="adam", loss=loss)
+    model.compile(optimizer="adam", loss=loss)
 
-checkpointDir = "./trainingCheckpoints"     # Speicherort für die Checkpoints
-checkpointPrefix = os.path.join(checkpointDir, "ckpt_{epoch}")      # Name der einzelnen Checkpoints
+    checkpointDir = "./trainingCheckpoints"     # Speicherort für die Checkpoints
+    checkpointPrefix = os.path.join(checkpointDir, "ckpt_{epoch}")      # Name der einzelnen Checkpoints
 
 
-# Speichern der Checkpoints
-checkpointCallback = tf.keras.callbacks.ModelCheckpoint(
-    filepath=checkpointPrefix,
-    save_weights_only=True
-)
+    # Speichern der Checkpoints
+    checkpointCallback = tf.keras.callbacks.ModelCheckpoint(
+        filepath=checkpointPrefix,
+        save_weights_only=True
+    )
 
-# Training
-history = model.fit(data, epochs=40, callbacks=[checkpointCallback])
+    # Training
+    history = model.fit(data, epochs=40, callbacks=[checkpointCallback])
